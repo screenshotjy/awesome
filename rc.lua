d85499054b091b04c12b58925cef1d8f87b5e52e
Jeffrey Yoo <jefyoo@gmail.com>
2018-02-25 22:53:28 -0600

    Add battery widget

--- rc.lua
+++ rc.lua
@@ -223,6 +223,7 @@ awful.screen.connect_for_each_screen(function(s)
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
+            require("battery-widget") {},
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
